15d397e3cb153c1933dfed13f2a451fa00b66d0c
Melvin <melvinflahaut@gmail.com>
2024-05-13 11:02:16 +0200

    corection intro

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -6,7 +6,7 @@ export default function Info() {
             <div className="Info_container">
                 <h1>Flahaut Melvin</h1>
                 <h2>Developpeur Front-End</h2>
-                <p>Je suis <span>Melvin</span>, un <span>developpeur Front-End</span> junior à la recherche actuellement d'une <span>alternance de 24mois</span> en tant que Developpeur Front-End</p>
+                <p>Je suis <span>Melvin</span>, un <span>développeur Front-End</span> junior à la recherche actuelle d'une <span>alternance de 24mois</span> en tant que Développeur Front-End</p>
                 <img src={PP} className='PP' alt='Flahaut Melvin'></img>
             </div>
         </section>
